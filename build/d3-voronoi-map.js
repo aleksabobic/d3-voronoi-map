49cf0b9e757ea61ee0344a4dda9da330d80cfa6c
Aleksandar <bobic.aleksandar92@gmail.com>
2018-06-21 12:18:57 +0200

    Add new build file

--- build/d3-voronoi-map.js
+++ build/d3-voronoi-map.js
@@ -165,7 +165,6 @@
   };
 
   function voronoiMap() {
-    console.log("hello from d3-voronoi-map");
     //begin: constants
     var DEFAULT_CONVERGENCE_RATIO = 0.01;
     var DEFAULT_MAX_ITERATION_COUNT = 50;
@@ -540,10 +539,16 @@
 
       //begin: extract weights
       weights = data.map(function (d, i, arr) {
+
+        initialPosition =
+          d.polygon ?
+          d.polygon.site.originalObject.data.initialPosition :
+          initialPosition(d, i, arr, _voronoiMap);
+
         return {
           index: i,
           weight: Math.max(weight(d), minAllowedWeight),
-          initialPosition: initialPosition(d, i, arr, _voronoiMap),
+          initialPosition: initialPosition,
           initialWeight: initialWeight(d, i, arr, _voronoiMap),
           originalData: d
         };
